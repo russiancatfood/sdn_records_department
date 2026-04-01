ecab2c3a6b9b521de1ba5fd9f53ba15bd6abf3b5
Maks Zolin <mzolin@apple.com>
2026-04-01 01:12:31 -0700

    Rendered milestones with descriptions, improved redability, OG preview, shortened permalink

--- gvas_parser/permalink_map.js
+++ gvas_parser/permalink_map.js
@@ -1,38 +1,182 @@
 export const stats_to_hash_map = {
-    'NV_Robert Hero': 'rh',
-    'BV_Dropped Him': 'dh',
-    'BV_Hacking Success': 'hs',
-    'NV_Robert Antihero': 'ra',
-    'BV_Punt Him': 'ph',
-    'BV_Bucket Success': 'bs',
-    'BV_Attack Reporter': 'ar',
-    'BV_Threw Water': 'tw',
-    'BV_Kissed Blonde Blazer': 'kbb',
-    'BV_Played Tutorial Well': 'ptw',
-    'BV_Played Tutorial Poorly': 'ptp',
-    'BV_AcceptableShiftRating': 'asr',
-    'BV_102 Fist Bump Royd': 'fbr',
-    'NV_RobertMentorCounter': 'rmc',
-    'BV_Chose Disarm Granny': 'cdg',
-    'BV_Kick Out Golem': 'kog',
-    'BV_Cut Sonar': 'cs',
-    'NV_SweetExitCounter': 'sec',
-    'BV_Chose Waterboy': 'cw',
-    'BV_Chose Invisigal': 'ci',
-    'BV_Told Team Robert': 'ttr',
-    'BV_Chose Beer': 'cb',
-    'BV_Chose Twinkies': 'ct',
-    "BV_She's gone": 'sg',
-    'BV_Blamed Visi': 'bv',
-    'BV_Leaned In': 'li',
-    'BV_Said Track Star': 'sts',
-    'BV_Glassed him': 'gh',
-    'BV_Failed Last Dispatch': 'fld',
-    'BV_Untie Invisigal': 'ui',
-    'BV_Chose_ClusterBombs': 'ccb',
-    'BV_Gave Both Pulses': 'gbp',
-    'BV_Robert Killed Shroud': 'rks',
-    'BV_Save ZTeamer': 'sz',
-    'BV_104 Kissed Blazer': 'kb',
-    'BV_BackedOutBlazer': 'bob'
+    'NV_Robert Hero': {
+        'id':0,
+        'type': 'integer',
+        'label':'rh'
+    },
+    'BV_Dropped Him': {
+        'id':4,
+        'type': 'boolean',
+        'label':'dh',
+    },
+    'BV_Hacking Success': {
+        'id':5,
+        'type': 'boolean',
+        'label':'hs',
+    },
+    'NV_Robert Antihero': {
+        'id':6,
+        'type': 'integer',
+        'label':'ra',
+    },
+    'BV_Punt Him': {
+        'id':7,
+        'type': 'boolean',
+        'label':'ph',
+    },
+    'BV_Bucket Success': {
+        'id':8,
+        'type': 'boolean',
+        'label':'bs',
+    },
+    'BV_Attack Reporter': {
+        'id':9,
+        'type': 'boolean',
+        'label':'ar',
+    },
+    'BV_Threw Water': {
+        'id':10,
+        'type': 'boolean',
+        'label':'tw',
+    },
+    'BV_Kissed Blonde Blazer': {
+        'id':11,
+        'type': 'boolean',
+        'label':'kbb',
+    },
+    'BV_Played Tutorial Well': {
+        'id':12,
+        'type': 'boolean',
+        'label':'ptw',
+    },
+    'BV_Played Tutorial Poorly': {
+        'id':13,
+        'type': 'boolean',
+        'label':'ptp',
+    },
+    'BV_AcceptableShiftRating': {
+        'id':14,
+        'type': 'boolean',
+        'label':'asr',
+    },
+    'BV_102 Fist Bump Royd': {
+        'id':15,
+        'type': 'boolean',
+        'label':'fbr',
+    },
+    'NV_RobertMentorCounter': {
+        'id':2,
+        'type': 'integer',
+        'label':'rmc',
+    },
+    'BV_Chose Disarm Granny': {
+        'id':16,
+        'type': 'boolean',
+        'label':'cdg',
+    },
+    'BV_Kick Out Golem': {
+        'id':17,
+        'type': 'boolean',
+        'label':'kog',
+    },
+    'BV_Cut Sonar': {
+        'id':18,
+        'type': 'boolean',
+        'label':'cs',
+    },
+    'NV_SweetExitCounter': {
+        'id':3,
+        'type': 'integer',
+        'label':'sec',
+    },
+    'BV_Chose Waterboy': {
+        'id':19,
+        'type': 'boolean',
+        'label':'cw',
+    },
+    'BV_Chose Invisigal': {
+        'id':20,
+        'type': 'boolean',
+        'label':'ci',
+    },
+    'BV_Told Team Robert': {
+        'id':21,
+        'type': 'boolean',
+        'label':'ttr',
+    },
+    'BV_Chose Beer': {
+        'id':22,
+        'type': 'boolean',
+        'label':'cb',
+    },
+    'BV_Chose Twinkies': {
+        'id':23,
+        'type': 'boolean',
+        'label':'ct',
+    },
+    "BV_She's gone": {
+        'id':24,
+        'type': 'boolean',
+        'label':'sg',
+    },
+    'BV_Blamed Visi': {
+        'id':25,
+        'type': 'boolean',
+        'label':'bv',
+    },
+    'BV_Leaned In': {
+        'id':26,
+        'type': 'boolean',
+        'label':'li',
+    },
+    'BV_Said Track Star': {
+        'id':27,
+        'type': 'boolean',
+        'label':'sts',
+    },
+    'BV_Glassed him': {
+        'id':28,
+        'type': 'boolean',
+        'label':'gh',
+    },
+    'BV_Failed Last Dispatch': {
+        'id':29,
+        'type': 'boolean',
+        'label':'fld',
+    },
+    'BV_Untie Invisigal': {
+        'id':30,
+        'type': 'boolean',
+        'label':'ui',
+    },
+    'BV_Chose_ClusterBombs': {
+        'id':31,
+        'type': 'boolean',
+        'label':'ccb',
+    },
+    'BV_Gave Both Pulses': {
+        'id':32,
+        'type': 'boolean',
+        'label':'gbp',
+    },
+    'BV_Robert Killed Shroud': {
+        'id':33,
+        'type': 'boolean',
+        'label':'rks',
+    },
+    'BV_Save ZTeamer': {
+        'id':34,
+        'type': 'boolean',
+        'label':'sz',
+    },
+    'BV_104 Kissed Blazer': {
+        'id':35,
+        'type': 'boolean',
+        'label':'kb',
+    },
+    'BV_BackedOutBlazer': {
+        'id':36,
+        'type': 'boolean',
+        'label':'bob'
+    }
 }
---
+++ stats_catalog.js
@@ -0,0 +1,459 @@
+export const player_journey = {
+    "Episode 1" : {
+        'BV_Dropped Him' : {
+            0: {
+                'title': "Pulled the goon back up",
+                'category': 'career',
+                'image': '101_pull_him_back_up.jpg'
+            },
+            1: {
+                'title': "Let the goon drop",
+                'category': 'career',
+                'image': '101_let_him_drop.jpg'
+            }
+        },
+        'BV_Punt Him' : {
+            0 : {
+                'title' : 'Stomped on Toxic',
+                'category': 'career',
+                'image' : '101_stomp_him.jpg'
+            },
+            1 : {
+                'title' : 'Punted Toxic',
+                'category': 'career',
+                'image' : '101_punt_him.jpg'
+            }
+        },
+        'BV_Bucket Success' : {
+            0 : {
+                'title' : 'Missed the chance to drop the bucket on Toxic',
+                'category': 'career',
+                'image' : '101_bucket.jpg'
+            },
+            1: {
+                'title' : 'Dropped the bucket on Toxic',
+                'category': 'career',
+                'image' : '101_bucket.jpg'
+            }
+        },
+        'BV_Attack Reporter' : {
+            0 : {
+                'title' : 'Handled the press conference with grace',
+                'category': 'career',
+                'image' : '101_answered_reporter.jpg'
+            },
+            1: {
+                'title' : 'Assaulted Charles Kingsley',
+                'category': 'career',
+                'image' : '101_attacked_reporter.jpg'
+            }
+        },
+        'BV_Threw Water' : {
+            0 : {
+                'title' : 'Set Flambae on fire',
+                'category': 'career',
+                'image' : '101_alcohol.jpg'
+            },
+            1 : {
+                'title' : 'Extinguished the situation',
+                'category': 'career',
+                'image' : '101_water.jpg'
+            }
+        },
+        'BV_Kissed Blonde Blazer' : {
+            0 : {
+                'title' : 'Let the moment pass',
+                'category': 'romantic',
+                'image' : '101_let_the_moment_pass.jpg'
+            },
+            1 : {
+                'title' : 'Went in for the kiss',
+                'category': 'romantic',
+                'image' : '101_billboard_kiss.jpg'
+            }
+        },
+        'BV_Played Tutorial Well' : {
+            1 : {
+                'title' : 'Aced the tutorial shift',
+                'category': 'career',
+                'image' : '101_went_well.jpg'
+            },
+            0 : {
+                'stat' : 'BV_Played Tutorial Poorly',
+                'values' : {
+                    1 : {
+                        'title': 'Bombed the tutorial shift',
+                        'category': 'career',
+                        'image' : '101_went_poorly.jpg'
+                    },
+                    0: {
+                        'title': 'Barely passed the tutorial shift',
+                        'category': 'career',
+                        'image' : '101_barely_passed.jpg'
+                    }
+                }   
+            }
+        },
+    },
+    "Episode 2" : {
+        'BV_102 Fist Bump Royd' : {
+            1 : {
+                'title' : "Fist bumped Royd",
+                'category': 'career',
+                'image' : '102_royd_bro_fist.jpg'
+            },
+            0 : {
+                'title' : "Left Royd haging",
+                'category': 'career',
+                'image' : '102_left_royd_hanging.jpg'
+            }
+        },
+        'BV_Chose Disarm Granny' : {
+            0 : {
+                'title' : "Ordered Visi to disarm LightningStrike",
+                'category': 'career',
+                'image' : '102_disarm_lightningstrike.jpg'
+            },
+            1 : {
+                'title' : "Ordered Visi to disarm Granny",
+                'category': 'career',
+                'image' : '102_disarm_granny.jpg'
+            }
+        },
+        'BV_Blamed Visi' : {
+            0 : {
+                'title' : "Covered for Visi",
+                'category': 'career',
+                'image' : '102_covered_for_visi.jpg'
+            },
+            1 : {
+                'title' : "Reported Invisigal for punching you",
+                'category': 'career',
+                'image' : '102_reported_invisigal.jpg'
+            }
+        }
+    },
+    "Episode 3" : {
+        'BV_Kick Out Golem' : {
+            0 : {
+                'title' : "Gave Golem a chair",
+                'category': 'career',
+                'image' : '103_golem_chair.jpg'
+            },
+            1 : {
+                'title' : "Kicked Golem out",
+                'category': 'career',
+                'image' : '103_golem_leave.jpg'
+            }
+        },
+        'BV_Cut Sonar' : {
+            0 : {
+                'title' : "Cut Coupé from the team",
+                'category': 'career',
+                'image' : '103_cut_coupe.jpg'
+            },
+            1 : {
+                'title' : "Cut Sonar from the team",
+                'category': 'career',
+                'image' : '103_cut_sonar.jpg'
+            }
+        }
+    },
+    'Episode 4' : {
+        'BV_Chose Waterboy' : {
+            0 : {
+                'title' : "Added Phenomaman to the team",
+                'category': 'career',
+                'image' : '104_phenomaman.jpg'
+            },
+            1 : {
+                'title' : "Added Waterboy to the team",
+                'category': 'career',
+                'image' : '104_waterboy.jpg'
+            }
+        },
+        'BV_Chose Invisigal' : {
+            0 : {
+                'title' : "Had dinner with Mandy",
+                'category': 'romantic',
+                'image' : '104_dinner_with_mandy.jpg'
+            },
+            1 : {
+                'title' : "Went to the movies with Visi",
+                'category': 'romantic',
+                'image' : '104_movies_with_visi.jpg'
+            }
+        },
+        'BV_104 Kissed Blazer' : {
+            0 : {
+                'title' : "Didn't kiss Mandy during your dinner date",
+                'category': 'romantic',
+                'image' : '104_no_kiss.jpg'
+            },
+            1 : {
+                'title' : "Kissed Mandy during your dinner date",
+                'category': 'romantic',
+                'image' : '104_kiss.jpg'
+            }
+        }
+    },
+    "Episode 5" : {
+        'BV_BackedOutBlazer' : {
+            0 : {
+                'title' : "Agreed to a second date",
+                'category': 'romantic',
+                'image' : '105_second_date.jpg'
+            },
+            1 : {
+                'title' : "Pumped the breaks on the romance",
+                'category': 'romantic',
+                'image' : '105_pump_the_brakes.jpg'
+            }
+        },
+        'BV_Told Team Robert' : {
+            0 : {
+                'title' : "Revealed your identity to the Z-Team",
+                'category': 'career',
+                'image' : '105_mecha_man.jpg'
+            },
+            1 : {
+                'title' : "Kept your identity a secret",
+                'category': 'career',
+                'image' : '105_robert.jpg'
+            }
+        }   
+    },
+    "Episode 7" : {
+        'BV_Chose Twinkies' : {
+            1 : {
+                'title' : 'Asked TrackStar for Twinkies',
+                'category': 'career',
+                'image' : '107_twinkies.jpg'
+            },
+            0 : {
+                'stat' : 'BV_Chose Beer',
+                'values' : {
+                    1 : {
+                        'title': 'Asked Track Star for beer',
+                        'category': 'career',
+                        'image' : '107_beer.jpg'
+                    },
+                    0: {
+                        'title': 'Asked Track Star for a cigar',
+                        'category': 'career',
+                        'image' : '107_cigar.jpg'
+                    }
+                }   
+            }
+        },
+        "BV_She's gone" : {
+            0 : {
+                'title' : "Defended Invisigal",
+                'category': 'career',
+                'image' : '107_invisigals_fate_all_choices.jpg'
+            },
+            1 : {
+                'title' : "Cut Invisigal",
+                'category': 'career',
+                'image' : '107_invisigals_fate_all_choices.jpg'
+            }
+        },
+        'BV_Leaned In' : {
+            1 : {
+                'title' : "Leaned into the kiss",
+                'category': 'romantic',
+                'image' : '107_leaned_in.jpg'
+            },
+            0 : {
+                'title' : "Didn't reciprocate Visi's affection",
+                'category': 'romantic',
+                'image' : '107_leaned_out.jpg'
+            }
+        },
+        'BV_Said Track Star' : {
+            1 : {
+                'title' : "Honored Track Star in the press release",
+                'category': 'career',
+                'image' : '107_pr_statement.jpg'
+            },
+            0 : {
+                'title' : "Blamed Invisigal in the press release",
+                'category': 'career',
+                'image' : '107_pr_statement.jpg'
+            }
+        },
+        'BV_Glassed him' : {
+            1 : {
+                'title' : "Glassed the bar keep",
+                'category': 'career',
+                'image' : '107_glassed_him.jpg'
+            },
+            0 : {
+                'title' : "Kept your cool, and let the insults go",
+                'category': 'career',
+                'image' : '107_let_it_go.jpg'
+            }
+        }
+    },
+    "Episode 8" : {
+        'BV_Failed Last Dispatch' : {
+            1 : {
+                'stat' : 'BV_Cut Sonar',
+                'values' : {
+                    1 : {
+                        'title': 'Failed to defeat Sonar',
+                        'category': 'career',
+                        'image' : '108_failed_sonar.jpg'
+                    },
+                    0: {
+                        'title': 'Failed to defeat Coupé',
+                        'category': 'career',
+                        'image' : '108_failed_coupe.jpg'
+                    }
+                }   
+            },
+            0 : {
+                'stat' : 'BV_Cut Sonar',
+                'values' : {
+                    1 : {
+                        'title': 'Defeated Sonar',
+                        'category': 'career',
+                        'image' : '108_beat_sonar.jpg'
+                    },
+                    0: {
+                        'title': 'Defeated Coupé',
+                        'category': 'career',
+                        'image' : '108_beat_coupe.jpg'
+                    }
+                }   
+            }
+        },
+        'BV_Untie Invisigal' : {
+            1 : {
+                'title' : "Trusted Invisigal",
+                'category': 'career',
+                'image' : '108_untie_her.jpg'
+            },
+            0 : {
+                'title' : "Didn't trust Invisigal",
+                'category': 'career',
+                'image' : '108_go_it_alone.jpg'
+            }
+        },
+        'BV_Chose_ClusterBombs' : {
+            1 : {
+                'title' : "Felt Cluster Bombs were justified",
+                'category': 'career',
+                'image' : '108_cluster_bombs.jpg'
+            },
+            0 : {
+                'title' : "Resorted to Plasma Nets",
+                'category': 'career',
+                'image' : '108_plasma_nets.jpg'
+            }
+        },
+        'BV_Gave Both Pulses' : {
+            1 : {
+                'title' : "Gave Shroud both Pulses",
+                'category': 'career',
+                'image' : '108_both_pulses.jpg'
+            },
+            0 : {
+                'title' : "Gave Shroud the real Pulse (eventually)",
+                'category': 'career',
+                'image' : '108_real_pulse.jpg'
+            }
+        },
+        'BV_Robert Killed Shroud' : {
+            1 : {
+                'title' : "Choked Shroud to death, exacting your revange",
+                'category': 'career',
+                'image' : '108_killed_shroud.jpg'
+            },
+            0 : {
+                'stat' : 'CON_VisiHero',
+                'values' : {
+                    1: {
+                        'title' : "Spared Shroud, showing him mercy",
+                        'category': 'career',
+                        'image' : '108_spared_shroud.jpg'
+                    },
+                    0: {
+                        'title' : "Helplessly watched as Visi stabbed Shrowd",
+                        'category': 'career',
+                        'image' : '108_visi_killed.jpg'
+                    }
+                }
+            }
+        },
+        'BV_Save ZTeamer' : {
+            1 : {
+                'stat' : 'BV_Cut Sonar',
+                'values' : {
+                    1 : {
+                        'title': 'Welcomed Sonar back onto the Z-Team',
+                        'category': 'career',
+                        'image' : '108_pardoned_sonar.jpg'
+                    },
+                    0: {
+                        'title': 'Welcomed Coupé back onto the Z-Team',
+                        'category': 'career',
+                        'image' : '108_pardoned_coupe.jpg'
+                    }
+                }   
+            },
+            0 : {
+                'stat' : 'BV_Cut Sonar',
+                'values' : {
+                    1 : {
+                        'title': 'Let authorities deal with Sonar',
+                        'category': 'career',
+                        'image' : '108_jailed_sonar.jpg'
+                    },
+                    0: {
+                        'title': 'Let authorities deal with Coupé',
+                        'category': 'career',
+                        'image' : '108_jailed_coupe.jpg'
+                    }
+                }   
+            }
+        },
+        'CON_VisiHero' : {
+            "stat" : "NV_RobertMentorCounter",
+            "threshold" : 45,
+            1: {
+                'title': 'Successfully mentored Visi towards the path of a hero',
+                'category': 'career',
+                'image' : '108_visi_hero.jpg'
+            },
+            0: {
+                'title': 'Failed as a mentor for Visi, leading her to embrace villany',
+                'category': 'career',
+                'image' : '108_visi_evil.jpg'
+            }
+        },
+        'CON_Relationship' : {
+            "type" : "relationship",
+            "Blonde Blazer (Mandy)" : {
+                'title': 'You and Mandy are officialy an item',
+                'category': 'romantic',
+                'image' : 'overall_romance_blazer.jpg'
+            },
+            "Invisigal (Courtney)" : {
+                'title': 'You and Courtney are officialy an item',
+                'category': 'romantic',
+                'image' : 'overall_romance_visi.jpg'
+            },
+            "Under HR Review" : {
+                'title': 'Played the field of inter-office romance, making your life complicated',
+                'category': 'romantic',
+                'image' : 'overall_romance_both.jpg'
+            },
+            "None" : {
+                'title': 'Put your career first, and avoided any ineter-office relationships',
+                'category': 'romantic',
+                'image' : 'overall_romance_none.jpg'
+            }
+        }
+    }
+}
